297b1c2516f40ee842335a318df00bbfdf457d2f
Valentin Alejandro Gimenez <valentingimenez1909@gmail.com>
2023-06-06 18:36:49 -0300

    Update Conexion

--- src/Control/Conexion.java
+++ src/Control/Conexion.java
@@ -5,10 +5,35 @@
  */
 package Control;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author Valentin
  */
 public class Conexion {
-    
+
+    private static final String URL = "jdbc:mysql://localhost/";
+    private static final String USER = "root";
+    private static final String PASSWORD = "";
+    private static final String DATABASE_NAME = "gestionar";
+
+    private static Connection connection;
+
+    private Conexion() {
+    }
+
+    public static Connection getConnection() {
+        if (connection == null) {
+            try {
+                connection = DriverManager.getConnection(URL + DATABASE_NAME, USER, PASSWORD);
+            } catch (SQLException ex) {
+                JOptionPane.showMessageDialog(null, "No se pudo conectar a la base de datos :'" + DATABASE_NAME + "'" + ex.getMessage());
+            }
+        }
+        return connection;
+    }
 }
