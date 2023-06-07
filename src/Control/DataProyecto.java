781102559c60abc6a7279e0b7925a988a9aca579
Valentin Alejandro Gimenez <valentingimenez1909@gmail.com>
2023-06-06 22:15:08 -0300

    guardarProyecto

--- src/Control/DataProyecto.java
+++ src/Control/DataProyecto.java
@@ -5,10 +5,47 @@
  */
 package Control;
 
+import Modelo.Proyecto;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author Valentin
  */
 public class DataProyecto {
+    
+    private Connection con = null;
+    public DataProyecto() {
+        con = Conexion.getConnection();
+    }
+    
+    public void guardarProyecto(Proyecto proyecto){
+        String sql = "INSERT INTO proyecto (nombre, descripcion, fechaInicio, estado) VALUES (?, ?, ?, ?)";
+        try {
+            PreparedStatement ps = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            ps.setString(1, proyecto.getNombre());
+            ps.setString(2, proyecto.getDescripcion());
+            ps.setDate(3, Date.valueOf(proyecto.getFechaInicio()));
+            ps.setInt(4, proyecto.getEstado());
+            ps.executeUpdate();
+            ResultSet rs = ps.getGeneratedKeys();
+            if (rs.next()) {
+                proyecto.setIdProyecto(rs.getInt("idProyecto"));
+                JOptionPane.showMessageDialog(null, "Proyecto añadido con exito.");
+            }
+            ps.close();
+
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error al acceder a la tabla Proyecto" + ex.getMessage());
+        }
+
+    }
+    
     
 }
--- src/tpfinal/gestionar/TPFinalGestionAR.java
+++ src/tpfinal/gestionar/TPFinalGestionAR.java
@@ -1,21 +1,15 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package tpfinal.gestionar;
 
-/**
- *
- * @author Valentin
- */
+import Control.DataProyecto;
+import Modelo.Proyecto;
+import java.time.LocalDate;
 public class TPFinalGestionAR {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        DataProyecto pr= new DataProyecto();
+        
+        pr.guardarProyecto(new Proyecto("GestionAr","Tp final de lab",LocalDate.of(2001, 9, 19),3));
+        
     }
-    
+
 }
